8dc0d225b1a011ec78193657a8f0b607fc868e45
Taasir <70100152+MohammedTaasir@users.noreply.github.com>
2022-02-26 22:57:28 +0530

    Update Phrases.java

--- app/src/main/java/com/example/miwok/Phrases.java
+++ app/src/main/java/com/example/miwok/Phrases.java
@@ -25,24 +25,24 @@ public class Phrases extends AppCompatActivity {
 
         final ArrayList<Word> n = new ArrayList<Word>();
 
-        n.add(new Word("My","Lady",R.raw.taasir_n));
-        n.add(new Word("i love you","more than myself",R.raw.p2));
-        n.add(new Word("i have worked on myself","just for you",R.raw.p3));
-        n.add(new Word("this world is so devil","but god made someone for me who will love me forever",R.raw.p4));
-        n.add(new Word("you are in my heart","i see myself in you",R.raw.p5));
-        n.add(new Word("you are my focus","i don't wanna lose you",R.raw.p6));
-        n.add(new Word("being with you","will only make me better day by day",R.raw.p7));
-        n.add(new Word("dont care what others say","coz its our both life we will decide what we want",R.raw.p8));
-        n.add(new Word("just kiss and carass me","i am forever yours",R.raw.p9));
-        n.add(new Word("i have'nt got any head massage yet","i m sure you will give me",R.raw.p10));
-        n.add(new Word("dont worry i will worship u","just say me what you want",R.raw.p11));
-        n.add(new Word("if u will give me one","i will give you thousand",R.raw.p12));
-        n.add(new Word("i do like food","as much as i love you",R.raw.p13));
-        n.add(new Word("i do like abs","as much as my lips loves your lips",R.raw.p14));
-        n.add(new Word("i do like playing sports","as much as my tongue loves your tongue",R.raw.p15));
-        n.add(new Word("but short n sweet","i love nusrat shaikh",R.raw.p16));
-        n.add(new Word("stay with me","make me feel blessed",R.raw.p17));
-        n.add(new Word("my love with u won't end after sex","i swear on my dick",R.raw.p18));
+        n.add(new Word("My","s",R.raw.taasir_n));
+        n.add(new Word("s",R.raw.p2));
+        n.add(new Word("s",R.raw.p3));
+        n.add(new Word("s",R.raw.p4));
+        n.add(new Word("s",R.raw.p5));
+        n.add(new Word("s",R.raw.p6));
+        n.add(new Word("s",R.raw.p7));
+        n.add(new Word("s",R.raw.p8));
+        n.add(new Word("s",R.raw.p9));
+        n.add(new Word("se",R.raw.p10));
+        n.add(new Word("st",R.raw.p11));
+        n.add(new Word("sd",R.raw.p12));
+        n.add(new Word("su",R.raw.p13));
+        n.add(new Word("ss",R.raw.p14));
+        n.add(new Word("se",R.raw.p15));
+        n.add(new Word("sh",R.raw.p16));
+        n.add(new Word("sd",R.raw.p17));
+        n.add(new Word("s",R.raw.p18));
 
 
         //
